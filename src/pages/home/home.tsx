fcf97b18fb272a2c3e238ef96b65d8ac525f5f91
Gabriel-Lemuchi <lemuchigabriel@gmail.com>
2026-05-16 12:38:56 -0300

    Ajustando responsividade

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
                     <h1>Seu negócio no próximo nível digital</h1>
                     <h2>Sites profissionais, automações e serviços administrativos</h2>
                 </div>
-                <img src='/BQ_Home.png' width="580px"/>
+                <img src='/BQ_Home.png'/>
                 <div id="button_section">
                 <Link to="/about">
                     <button id="about_button">Saiba Mais:</button>
